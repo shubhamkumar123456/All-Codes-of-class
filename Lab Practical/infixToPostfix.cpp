b4c7c11f9ae68dc5b88aece5abc77896e53ed050
shubham kumar <clboy768@gmail.com>
2025-09-16 20:41:14 +0530

    updated infix code

--- Lab Practical/infixToPostfix.cpp
+++ Lab Practical/infixToPostfix.cpp
@@ -19,34 +19,37 @@ int precedence(char op){
 string infixToPostFix(string infix){
   stack<char>st;
   string postFix = "";
-  for(int i=0; i<infix.size();i++){
-        char ch = infix[i];
-        if(isalnum(ch)){
-            postFix = postFix+ch;
-        }
-        else if(ch=='('){
-            st.push(ch);
-        }
-        else if(ch==')'){
-            while(!st.empty() && st.top()!='('){
-                    postFix = postFix+st.top();
-                    st.pop();
-            }
-            st.pop();
-        }
-        else{
-            while(!st.empty() && st.top()!='(' && precedence(st.top()) > precedence(ch)){
-                postFix+=st.top();
-                st.pop();
-            }
-            st.push(ch);
-        }
-  }
-  while(!st.empty()){
-    postFix+= st.top();
-    st.pop();
-  }
-  return postFix;
+  for(int i=0; i<infix.size(); i++){
+    char ch = infix[i];
+    if(isalnum(ch)){
+      postFix = postFix+ch;
+    }
+    else if(ch=='('){
+      st.push(ch);
+    }
+    else if(ch==')'){
+      while(!st.empty() && st.top()!='('){
+        postFix = postFix + st.top();
+        st.pop();
+      }
+      st.pop();
+    }
+    else{
+      while(!st.empty()  && precedence(st.top())>= precedence(ch)){
+        postFix+=st.top();
+        st.pop() ;
+      }
+      st.push(ch);
+      
+      }
+    }
+    
+    while(!st.empty()){
+      postFix+= st.top();
+      st.pop();
+    }
+    return postFix;
+  
 }
 
 
